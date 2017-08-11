67ddb7e5b091af708a7d3231e026cb0e6a7a35f0
Damien Vergnet <darmo117@gmail.com>
2017-08-11 17:04:51 +0200

    refactor: removed useless .converter package

--- src/main/java/net/darmo_creations/unicode_converter/AboutDialog.java
+++ src/main/java/net/darmo_creations/unicode_converter/AboutDialog.java
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package net.darmo_creations.unicode_converter.converter;
+package net.darmo_creations.unicode_converter;
 
 import java.awt.BorderLayout;
 import java.awt.Desktop;

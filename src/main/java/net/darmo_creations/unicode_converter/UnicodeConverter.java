0c057f5e409a65bc5b4af09588898154bae55237
Damien Vergnet <darmo117@gmail.com>
2017-08-23 20:39:10 +0200

    changed version (1.0.1 -> 1.1)

--- src/main/java/net/darmo_creations/unicode_converter/UnicodeConverter.java
+++ src/main/java/net/darmo_creations/unicode_converter/UnicodeConverter.java
@@ -10,7 +10,7 @@ import net.darmo_creations.gui_framework.gui.ApplicationFrame;
 import net.darmo_creations.utils.version.Version;
 
 public class UnicodeConverter implements Application {
-  public static final Version CURRENT_VERSION = new Version(1, 0, 2, false);
+  public static final Version CURRENT_VERSION = new Version(1, 1, 0, false);
 
   @Override
   public String getName() {
